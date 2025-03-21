93128c127c9a549077f8dbabf7e65ef9bf62c8d1
wondimagagnzewudu <wondimagagnzewudu@gmail.com>
2025-03-21 15:58:50 +0300

    Removed incorrect Jekyll workflow

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -1,4 +1,4 @@
-import Reactt from "react";
+import Reactt  from "react";
 // import React, {useContext} from "react";
 import "./WorkExperience.scss";
 // import ExperienceCard from "../../components/experienceCard/ExperienceCard";
