ca8cf46e29f72562e141d95ca76dc20b83a686f2
wondimagagnzewudu <wondimagagnzewudu@gmail.com>
2025-03-21 15:39:35 +0300

    first commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+import {Fade} from "react-reveal";
+
+
+export default function Footer() {
+ 
+  return (
+    <Fade bottom duration={1000} distance="5px">
+
+    </Fade>
+  );
+}
